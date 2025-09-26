36b6b105b11bddb94f795fd521ac443ec56881db
Steven Gordon <s.d.gordon@cqu.edu.au>
2025-09-26 17:35:47 +1000

    Add --force to delete containers

--- server/vm-delete-containers.sh
+++ server/vm-delete-containers.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # Delete Docker containers manually
 
-docker rmi gns3/endhost:latest
-docker rmi gns3/openvswitch:latest
-docker rmi gns3/ipterm-base:latest
-docker rmi gns3/ipterm:latest
-docker rmi gns3/webterm:latest
-docker rmi cqugns3/ansible:latest
-docker rmi cqugns3/vpnrouter:latest
-docker rmi adosztal/net_toolbox:latest
+docker rmi --force gns3/endhost:latest
+docker rmi --force gns3/openvswitch:latest
+docker rmi --force gns3/ipterm-base:latest
+docker rmi --force gns3/ipterm:latest
+docker rmi --force gns3/webterm:latest
+docker rmi --force cqugns3/ansible:latest
+docker rmi --force cqugns3/vpnrouter:latest
+docker rmi --force adosztal/net_toolbox:latest
