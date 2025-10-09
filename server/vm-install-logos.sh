29769d08e88755bb8e00f7b7c8a010c57c2bb920
Steven Gordon <s.d.gordon@cqu.edu.au>
2025-10-09 21:41:02 +1000

    quiet on firefox logo download

--- server/vm-install-logos.sh
+++ server/vm-install-logos.sh
@@ -2,7 +2,7 @@
 # Install logos
 
 # Copy Firefox logo
-wget https://raw.githubusercontent.com/GNS3/gns3-registry/refs/heads/master/symbols/firefox.svg -O /home/gns3/firefox.svg
+wget -q https://raw.githubusercontent.com/GNS3/gns3-registry/refs/heads/master/symbols/firefox.svg -O /home/gns3/firefox.svg
 cp /home/gns3/firefox.svg ~/.venv/gns3server-venv/lib/python3.9/site-packages/gns3server/symbols/classic/
 
 # Other logos
